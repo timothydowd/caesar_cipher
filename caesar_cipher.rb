59add85a995ccc65f396e664ce74117555fc198a
dwdy250 <timdowd1984@gmail.com>
2018-07-16 22:35:49 +0100

    forgotten ruby

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,4 +1,14 @@
-cipher = {"a" => 1, "b" => 2, "c" => 3, "d" => 4, "e" => 5, "f" => 6, "g" => 7, "h" => 8, "i" => 9, "j" => 10, "k" => 11, "l" => 12, "m" => 13, "n" => 14, "o" => 15, "p" => 16, "q" => 17, "r" => 18, "s" => 19, "t" => 20, "u" => 21, "v" => 22, "w" => 23, "x" => 24, "y" => 25, "z" => 26}
+index = {"a" => 1, "b" => 2, "c" => 3, "d" => 4, "e" => 5, "f" => 6, "g" => 7, "h" => 8, "i" => 9, "j" => 10, "k" => 11, "l" => 12, "m" => 13, "n" => 14, "o" => 15, "p" => 16, "q" => 17, "r" => 18, "s" => 19, "t" => 20, "u" => 21, "v" => 22, "w" => 23, "x" => 24, "y" => 25, "z" => 26}
 
-def caesar_cipher(str,shift)
-  
+def encrypt(str,shift)
+  str = str.split("")
+  str.each do |letter|
+    puts letter
+    #index_no = index[letter] + 5
+    puts index[letter]
+  end
+
+
+end
+
+encrypt("toilet",5)

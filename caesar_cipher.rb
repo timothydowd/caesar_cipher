efbd8f77b315ecc6f187d2ae5efd75afa41e37b1
dwdy250 <timdowd1984@gmail.com>
2018-07-18 22:34:00 +0100

    working without spaces

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,16 +1,20 @@
-index = {"a" => 1, "b" => 2, "c" => 3, "d" => 4, "e" => 5, "f" => 6, "g" => 7, "h" => 8, "i" => 9, "j" => 10, "k" => 11, "l" => 12, "m" => 13, "n" => 14, "o" => 15, "p" => 16, "q" => 17, "r" => 18, "s" => 19, "t" => 20, "u" => 21, "v" => 22, "w" => 23, "x" => 24, "y" => 25, "z" => 26}
+HASH_INDEX = {"a" => 1, "b" => 2, "c" => 3, "d" => 4, "e" => 5, "f" => 6, "g" => 7, "h" => 8, "i" => 9, "j" => 10, "k" => 11, "l" => 12, "m" => 13, "n" => 14, "o" => 15, "p" => 16, "q" => 17, "r" => 18, "s" => 19, "t" => 20, "u" => 21, "v" => 22, "w" => 23, "x" => 24, "y" => 25, "z" => 26}
 
 str_array = []
 
 def encrypt(str,shift)
   str_array = str.split("")
-  str_array.each do |letter|
-    puts letter
-    #index_no = index[letter] + 5
-    puts index[letter]
+  str_array.map! do |letter|
+
+    index_no = HASH_INDEX[letter] + shift
+    letter = HASH_INDEX.key(index_no)
+
   end
 
+  puts str_array.join("")
 
 end
+
+
 
 encrypt("toilet",5)
